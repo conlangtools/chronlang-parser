1d56f0b0d89bcb93a02c42570946f757e845da1a
Fergus Bentley <fergusbentley@gmail.com>
2024-10-27 22:24:54 +0000

    Improve public API

--- mod.ts
+++ mod.ts
@@ -1,9 +1,21 @@
-import peggy from "peggy"
+import peggy, { GrammarError } from "peggy"
 
 export * as ast from "./ast/mod.ts"
 
+import { Stmt } from "./ast/statements.ts"
+
+type ParseResult =
+  | { ok: true, statements: Stmt[] }
+  | { ok: false, error: GrammarError }
+
 const parser = peggy.generate(Deno.readTextFileSync("./grammar.pegjs"))
 
-export function parse(source: string, sourceName: string) {
-  return parser.parse(source, { grammarSource: sourceName })
+export function parse(source: string, sourceName: string): ParseResult {
+  try {
+    const statements = parser.parse(source, { grammarSource: sourceName })
+    return { ok: true, statements }
+  } catch (error) {
+    return { ok: false, error }
+  }
+  
 }
