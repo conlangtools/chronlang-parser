c5d5ee04bca775a7924c0a7b0aced2ae3545cd60
faewd <git@faew.dev>
2024-10-28 10:36:04 +0000

    Export GrammerError

--- mod.ts
+++ mod.ts
@@ -1,14 +1,14 @@
 import peggy from "peggy";
-
-export * as ast from "./ast/mod.ts";
-
-import { Stmt } from "./ast/statements.ts";
+import type { Stmt } from "./ast/statements.ts";
 
 type ParseResult =
   | { ok: true; statements: readonly Stmt[] }
   | { ok: false; error: peggy.GrammarError };
 
 const parser = peggy.generate(Deno.readTextFileSync("./grammar.pegjs"));
+
+export * as ast from "./ast/mod.ts";
+export const { GrammarError } = peggy;
 
 export function parse(source: string, sourceName: string): ParseResult {
   try {
