95d572162c9a19449d82e0bbd3d4924912f59ef5
Fergus Bentley <fergusbentley@gmail.com>
2024-10-29 19:19:05 +0000

    Fix SyntaxError type

--- mod.ts
+++ mod.ts
@@ -2,21 +2,21 @@ import peggy from "peggy";
 import grammar from "./grammar.ts"
 import type { Stmt } from "./ast/statements.ts";
 
+const parser = peggy.generate(grammar);
+
 export * as ast from "./ast/mod.ts";
-export type SyntaxError = peggy.parser.SyntaxError;
+export const SyntaxError = parser.SyntaxError;
 
 type ParseResult =
   | { ok: true; statements: readonly Stmt[] }
   | { ok: false; error: SyntaxError };
-
-const parser = peggy.generate(grammar);
 
 export function parse(source: string, sourceName: string): ParseResult {
   try {
     const statements = parser.parse(source, { grammarSource: sourceName });
     return { ok: true, statements };
   } catch (error) {
-    if (error instanceof peggy.parser.SyntaxError) {
+    if (error instanceof SyntaxError) {
       return { ok: false, error };
     } else {
       throw error;
