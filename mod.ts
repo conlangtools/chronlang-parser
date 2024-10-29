d4459b7fb2eb936a7fc6dd18a1c5597392e5c60c
faewd <git@faew.dev>
2024-10-29 16:59:31 +0000

    Export SyntaxError instead of GrammarError

--- mod.ts
+++ mod.ts
@@ -2,21 +2,21 @@ import peggy from "peggy";
 import grammar from "./grammar.ts"
 import type { Stmt } from "./ast/statements.ts";
 
+export * as ast from "./ast/mod.ts";
+export type SyntaxError = peggy.parser.SyntaxError;
+
 type ParseResult =
   | { ok: true; statements: readonly Stmt[] }
-  | { ok: false; error: peggy.GrammarError };
+  | { ok: false; error: SyntaxError };
 
 const parser = peggy.generate(grammar);
-
-export * as ast from "./ast/mod.ts";
-export type GrammarError = peggy.GrammarError;
 
 export function parse(source: string, sourceName: string): ParseResult {
   try {
     const statements = parser.parse(source, { grammarSource: sourceName });
     return { ok: true, statements };
   } catch (error) {
-    if (error instanceof peggy.GrammarError) {
+    if (error instanceof peggy.parser.SyntaxError) {
       return { ok: false, error };
     } else {
       throw error;
