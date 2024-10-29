ee09ec92db3711710d4538120ddc22a79478f661
Fergus Bentley <fergusbentley@gmail.com>
2024-10-29 20:58:39 +0000

    Fix SyntaxError type

--- mod.ts
+++ mod.ts
@@ -5,18 +5,18 @@ import type { Stmt } from "./ast/statements.ts";
 const parser: peggy.Parser = peggy.generate(grammar);
 
 export * as ast from "./ast/mod.ts";
-export const SyntaxError = parser.SyntaxError;
+export type SyntaxError = peggy.parser.SyntaxError;
 
 type ParseResult =
   | { ok: true; statements: readonly Stmt[] }
-  | { ok: false; error: SyntaxError };
+  | { ok: false; error: peggy.parser.SyntaxError };
 
 export function parse(source: string, sourceName: string): ParseResult {
   try {
     const statements = parser.parse(source, { grammarSource: sourceName });
     return { ok: true, statements };
   } catch (error) {
-    if (error instanceof SyntaxError) {
+    if (error instanceof peggy.parser.SyntaxError) {
       return { ok: false, error };
     } else {
       throw error;
