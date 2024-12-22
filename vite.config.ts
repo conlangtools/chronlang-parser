39604d817d12022517b65b023fe0df610e77c517
fergcb <fergusbentley@gmail.com>
2024-12-22 14:50:04 +0000

    Emit .d.ts during bundling

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { resolve } from 'path'
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
+import dts from "vite-plugin-dts"
 
 export default defineConfig({
+  plugins: [dts({ rollupTypes: true })],
   build: {
     lib: {
       entry: resolve(__dirname, './src/mod.ts'),
